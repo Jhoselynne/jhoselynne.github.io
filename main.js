fb8396e0a30b8edddfcb2d9de4b17ed70798a3a7
Jhoselynne <jhoselynne@hotmail.com>
2021-09-15 15:24:33 +0200

    Initial web commit
    The startup commit

---
+++ main.js
@@ -0,0 +1,26 @@
+document.getElementById("Knappen1").addEventListener("click", function () {
+    console.log("Fel svar");
+document.getElementById("answer").innerHTML="<h1> Fel svar :( </h1>";
+})
+
+// function knapp1() {
+    // console.log("Fel svar");
+// }
+
+document.getElementById("Knappen2").addEventListener("click", function () {
+    console.log("Rätt!");
+document.getElementById("answer").innerHTML="<h1> Rätt! :) </h1>";
+})
+
+// function knapp2() {
+    // console.log("Rätt! :)");
+// }
+
+document.getElementById("Knappen3").addEventListener("click", function () {
+    console.log("Fel svar");
+document.getElementById("answer").innerHTML="<h1> Fel svar :( </h1>";
+})
+
+// function knapp3() {
+    // console.log("Fel svar :(");
+// }
